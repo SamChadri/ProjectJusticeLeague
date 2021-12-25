6c862ddccab2470289c208da12445b251e1be4c6
Samuel Chadri <BEATFREAK@Sams-MBP.attlocal.net>
2021-12-25 03:39:53 -0600

    All I Want for Christmas...

--- src/xo_music.js
+++ src/xo_music.js
@@ -143,12 +143,100 @@ $(document).ready(function(){
 
 
 
-
-
-
-
-
     });
+    var featurette_height = $('.feature-pic').height();
+    var featurette_width = $('.feature-pic').width();
+    var player_ready = false;
+    var player_state = 'PLAYING';
+
+    $('#bf-1').click(function(){ playerButtonOnClick(this,true); });
+    function onPlayerReady(event){
+        var id = event.target.getIframe().id;
+        var num_id = id.substring(id.length - 1);
+        console.log('Playing video');
+        console.log(event.target);
+        event.target.playVideo();
+        $(`#${id}`).hover(
+            function(){
+                if (event.target.getPlayerState() == YT.PlayerState.PAUSED){
+                    event.target.playVideo();
+                }
+            },
+            function(){
+                if(event.target.getPlayerState() == YT.PlayerState.PLAYING){
+                    event.target.pauseVideo();
+                }
+            });
+    }
+    function onPlayerStateChange(event){}
+
+    function createLocalVideo(width, height){
+        var video = document.createElement('video');
+        var source = document.createElement('source');
+
+        source.type = 'video/mp4';
+        source.src = '../pics/XO_MUSIC_TRAILER_MP4.mp4';
+
+        video.autoplay = true;
+        video.setAttribute("controls", "controls");
+        video.height = height;
+        video.width = featurette_width;
+        video.style.objectFit = 'fill';
+
+        video.appendChild(source)
+        return video
+    }
+
+    function playerButtonOnClick(curr_obj, local_vid){
+        //TODO: Add in functionality for React later on.
+        
+        var id = $(curr_obj).attr('id');
+        var num_id = id.substring(id.length - 1);
+        id = `info-div`
+        var fheight = $('.feature-pic').height();
+        var fwidth = $('.feature-pic').width();
+        console.log(`Height: ${fheight}, `);
+        console.log(`Width: ${fwidth}`);
+        if (local_vid == true){
+            var video = createLocalVideo(fwidth, fheight);
+            $('#info-div').html(video);
+            player_state = 'PLAYING';
+            $("#info-div").hover(
+                function(){
+                    if (player_state == 'PAUSED'){
+                        video.play();
+                        player_state = 'PLAYING';
+                    }
+                },
+                function(){
+                    if(player_state == 'PLAYING'){
+                        video.pause();
+                        player_state = 'PAUSED';
+                    }
+                });
+            
+        }else{
+            var videoId = '';
+            console.log(id);
+            if (id == 'vid-test2'){
+                videoId = '1ZHTurVaGus';
+            }else{
+                videoId = '5ngeO703yA4';
+            }  
+            var player = new YT.Player(id, {
+                height: fheight,
+                width: fwidth,
+                host:`${window.location.protocol}//www.youtube.com`,
+                videoId: videoId,//TrailerCode Goes here,
+                events: {
+                    'onReady': onPlayerReady,
+                    'onStateChange': onPlayerStateChange
+                }
+            });
+
+        }
+    }
+    
 
 
     $(window).scroll(function() {
